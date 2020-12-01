54014e79fce3b04776b16476a2a00ec98c0fdd30
Kristen <43305741+KISMET-INC@users.noreply.github.com>
2020-11-30 18:14:22 -0800

    update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -157,7 +157,6 @@
       // if(data.responseText) {
       //   error_msg += data.responseText;
       // }
-    
       this_form.find('.loading').slideUp();
       this_form.find('.sent-message').slideDown();
       this_form.find("input:not(input[type=submit]), textarea").val('');
